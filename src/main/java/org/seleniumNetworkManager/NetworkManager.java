35f19d3c16b05e62bea769f598a65ed07a000b79
Timothy A <albantimothy@gmail.com>
2023-10-10 22:51:24 -0400

    Updating the InvalidDriverException and the response received and loading complete events

--- src/main/java/org/seleniumNetworkManager/NetworkManager.java
+++ src/main/java/org/seleniumNetworkManager/NetworkManager.java
@@ -4,9 +4,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.bidi.BiDi;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v115.network.model.RequestId;
 import org.openqa.selenium.devtools.v115.network.Network;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.seleniumNetworkManager.exceptions.InvalidDriverException;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
@@ -20,7 +22,7 @@ public class NetworkManager {
     private BiDi biDiTools;
     private static List<String> browsers =  Arrays.asList(new String[] {"chrome"});
 
-    NetworkManager(WebDriver driver){
+    NetworkManager(WebDriver driver) throws InvalidDriverException {
 
         String browserName;
         ChromeDriver cd;
@@ -33,14 +35,22 @@ public class NetworkManager {
         }else if (browserName.equals("firefox")){
             fd = (FirefoxDriver)driver;
             biDiTools = fd.getBiDi();
+        }
+        else if(browserName.toLowerCase().equals("edge")){
 
+        }
+        else if(browserName.toLowerCase().equals("safari")){
 
+        }else{
+            throw new InvalidDriverException();
         }
     }
 
     public void startRecording(){
+
+        final RequestId[] requestIds = new RequestId[1];
         devTools.createSession();
-        devTools.send(Network.enable(Optional.empty(),Optional.empty(), Optional.empty()))
+        devTools.send(Network.enable(Optional.empty(),Optional.empty(), Optional.empty()));
 
         devTools.addListener(Network.responseReceived(), responseReceived -> {
             String requestId, url;
@@ -54,13 +64,47 @@ public class NetworkManager {
             }
         });
 
-        devTools.addListener(Network.loadingFinished(), loadingFinished -> {
-            String requestId, body;
-            requestId = loadingFinished.getRequestId().toString();
-            Network.getResponseBody(loadingFinished.getRequestId());
+        devTools.addListener(Network.webSocketCreated(), handler->{
 
         });
 
+        devTools.addListener(Network.loadingFinished(), loadingFinished -> {
+            String requestId, body, url;
+            requestId = body = url = null;
+            try{
+                requestIds[0] = loadingFinished.getRequestId();
+                requestId = requestIds[0].toString();
+                url = urlMap.get(requestId);
+                body = Network.getResponseBody(requestIds[0]).toString();
+                bodyMap.put(url, body);
+
+            }catch(Exception e){
+                System.out.println(
+                        "The request Id is: "+requestId+
+                        "\nThe current url: "+ url+
+                        "\n" + e);
+            }
+
+
+        });
+
+    }
+
+    /**
+     *
+     * @param endpoint
+     * @return The body of the request
+     */
+    public String getBody(String endpoint){
+        String body;
+        try{
+            body = bodyMap.get(endpoint);
+
+        }catch (NullPointerException npe){
+            System.out.println(""+npe);
+            return null;
+        }
+        return body;
     }
     public void stopRecording(){
         devTools.disconnectSession();
--- src/main/java/org/seleniumNetworkManager/exceptions/InvalidDriverException.java
+++ src/main/java/org/seleniumNetworkManager/exceptions/InvalidDriverException.java
@@ -3,12 +3,15 @@ package org.seleniumNetworkManager.exceptions;
 import net.bytebuddy.implementation.bind.MethodDelegationBinder;
 
 public class InvalidDriverException extends Exception{
-    InvalidDriverException(){
+    public InvalidDriverException(){
         super();
     }
-    InvalidDriverException(String message, String browser){
+    public InvalidDriverException(String message){
         super(message);
-        System.out.println("The current");
+    }
+    public InvalidDriverException(String message, String browser){
+        super(message);
+        System.out.println("The selected browser is incorrect: "+ browser);
     }
 
 
